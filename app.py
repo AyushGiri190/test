5542f9684118f159d32efcfa263745c337a106e8
AyushGiri190 <giriayush190@gmail.com>
2025-11-10 12:16:33 +0530

    changed

--- app.py
+++ app.py
@@ -2,7 +2,7 @@ from flask import Flask, request, jsonify
 from flask_cors import CORS
 from PIL import Image
 from lungcancer import predict_image_lung
-from tuberculosis import predict_image_tuber
+#from tuberculosis import predict_image_tuber
 from braincancer import predict_image_brain
 from Breast_cancer import predict_image_breast
 #from response import get_chatbot_response
